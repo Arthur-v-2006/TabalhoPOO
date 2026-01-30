53e6c5af23afc53605eb8b8007c7a75bdf968e01
Arthur-v-2006 <Arthur.vinicius.l.2006@gmail.com>
2026-01-30 17:58:25 -0300

    ajustando erros

--- Cliente.cs
+++ Cliente.cs
@@ -1,35 +1,35 @@
 public class Cliente : Usuario{
-    private string Empresa;
-    private string Telefone;
-    private string CpfCnpj;
-    private string Endereco;
+    private string empresa;
+    private string telefone;
+    private string cpfCnpj;
+    private string endereco;
 
-    public Cliente( int idUsuario, string nome, string email, string senha, string empresa, string telefone,string cpfCnpj, string endereco)
-    : base(idUsuario, nome, email, senha){
-        this.Empresa = empresa;
-        this.Telefone = telefone;
-        this.CpfCnpj = cpfCnpj;
-        this.Endereco = endereco;
+    public Cliente(int idUsuario, string nome, string email, string senha, string empresa, string telefone, string cpfCnpj, string endereco)
+        : base(idUsuario, nome, email, senha){
+        this.empresa = empresa;
+        this.telefone = telefone;
+        this.cpfCnpj = cpfCnpj;
+        this.endereco = endereco;
     }
 
     public string Empresa{
-        get { return empresa;}
-        set { empresa = value}
+        get { return empresa; }
+        set { empresa = value; }
     }
 
     public string Telefone{
-        get { return telefone;}
-        set { telefone = value}
+        get { return telefone; }
+        set { telefone = value; }
     }
 
     public string CpfCnpj{
-        get { return cpfCnpj;}
-        set { cpfCnpj = value}
+        get { return cpfCnpj; }
+        set { cpfCnpj = value; }
     }
 
     public string Endereco{
-        get { return endereco;}
-        set { endereco = value}
+        get { return endereco; }
+        set { endereco = value; }
     }
 
     public void AbrirChamado(string titulo, string descricao){
@@ -40,18 +40,18 @@ public class Cliente : Usuario{
     }
 
     public void AcompanharChamado(int idChamado){
-        Console.WriteLine($"Cliente estar acompanhado o chamado #{idChamado}");
+        Console.WriteLine($"Cliente {Nome} está acompanhando o chamado #{idChamado}");
         Console.WriteLine($"Contato: {telefone}");
     }
 
-    public void AvaliarAtendimento(){
+    public void AvaliarAtendimento(int idChamado, int nota, string comentario){
         Console.WriteLine($"Cliente {Nome} avaliou atendimento do chamado #{idChamado}");
         Console.WriteLine($"Nota: {nota}/5");
         Console.WriteLine($"Comentário: {comentario}");
     }
 
     public void CancelarAtendimento(){
-        Console.WriteLine($"Cliente {Nome} cancelou o chamado")
-        Console.WriteLine($"Motivo: solicitação do cliente")
+        Console.WriteLine($"Cliente {Nome} cancelou o chamado.");
+        Console.WriteLine($"Motivo: solicitação do cliente.");
     }
 }
